c58093ecec8a176e53fca32613e2ce544b0e8a58
Eduardo Vivo <eduvivmed@alu.edu.gva.es>
2024-01-26 10:46:34 +0100

    intento de auth casi terminado

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,33 +1,10 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { Routes, RouterModule } from "@angular/router";
-import { NgApexchartsModule } from "ng-apexcharts";
-import { AuthComponent } from "./auth.component";
-import { SignInComponent } from "./auth-components/sign-in/sign-in.component";
+import { NgModule } from '@angular/core';
 
-
-const routes: Routes = [
-  {
-    path: "",
-    data: {
-      title: "Auth",
-      urls: [{ title: "Auth", url: "/auth" }, { title: "Auth" }],
-    },
-    component: AuthComponent,
-  },
-];
+import { HttpClientModule } from '@angular/common/http';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { AuthRoutingModule } from './auth-routing.module';
 
 @NgModule({
-  imports: [
-    FormsModule,
-    ReactiveFormsModule,
-    CommonModule,
-    RouterModule.forChild(routes),
-    NgApexchartsModule,
-  ],
-  declarations: [
-    SignInComponent
-  ],
+  imports: [AuthRoutingModule, HttpClientModule, AngularSvgIconModule.forRoot()],
 })
 export class AuthModule {}
